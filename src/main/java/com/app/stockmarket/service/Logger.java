b3850b1d0d8947b6053e9df566a578ab679cca3a
shivasr <shivasr@gmail.com>
2016-09-20 12:41:07 +0530

    Added Logger API

---
+++ src/main/java/com/app/stockmarket/service/Logger.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.app.stockmarket.service;
+
+/**
+ * @author sramanna
+ *
+ */
+public class Logger {
+	
+	public static void logDebugMessage(String message) {
+		Logger.logDebugMessage(message);
+	}
+
+}
